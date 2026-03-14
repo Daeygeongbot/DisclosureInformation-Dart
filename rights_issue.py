bfd49ce33edee11cbd6245600bbe0eb943d6a45b
Daeygeongbot <kevinlim@dgvc.co.kr>
2026-03-14 19:11:11 +0900

    Update rights_issue.py

--- rights_issue.py
+++ rights_issue.py
@@ -117,7 +117,7 @@ def to_int(val):
 
 def get_and_update_yusang():
     start_date = '20260101'
-    end_date = '20260131'
+    end_date = '20260111'
 
     print(f"{start_date} ~ {end_date} 유상증자 공시 탐색 중...")
 
